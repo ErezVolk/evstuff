a8f73db989c4be8227159eec05120a3bc7b0f99b
Erez Volk <erez@affectlayer.com>
2018-02-20 21:17:54 +0200

    Some kind of initial player

---
+++ play_random_sentece/play_random_sentence.py
@@ -0,0 +1,96 @@
+import os
+import random
+import re
+import subprocess
+
+import aqt
+
+try:
+    from PyQt5 import QtCore
+except ImportError:
+    from PyQt4 import QtCore
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+CORPORA = [
+    os.path.join(HERE, 'sentences_%02d.txt' % nn)
+    for nn in (1, 2, 3)
+]
+
+
+def play_random_sentence(word):
+    global count
+    for level, corpus in zip('abcdefg', CORPORA):
+        hits = run_grep(corpus, word)
+        if hits:
+            break
+
+    if not hits:
+        return
+
+    jpn, eng = random.choice(hits)
+    subprocess.call(['/usr/bin/say', '-v', random.choise(JPN_VOICES), jpn])
+    subprocess.call(['/usr/bin/say', '-v', random.choise(ENG_VOICES), eng])
+
+
+def run_grep(corpus, word):
+    output = run('/usr/bin/grep', word, corpus)
+    return [
+        line_to_hit(line)
+        for line in output.split('\n')
+        if line
+    ]
+
+
+def run(*cmdline):
+    return subprocess.check_output(cmdline)
+
+
+def line_to_hit(line):
+    m = re.match(r'^(\d+\*?)\t(.*)\t(\d+\*?)\t(.*)$', line)
+    return (m.group(2), m.group(4))
+
+
+def get_voices(lang):
+    output = run('/usr/bin/say', '-v', '?')
+    return [
+        line.split(' ', 1)[0]
+        for line in output.split('\n')
+        if lang in line
+    ]
+
+
+def evlog(*args):
+    with open('/tmp/erez.log', 'a+') as f:
+        for arg in args:
+            f.write(unicode(arg).encode('utf-8'))
+        f.write('\n')
+
+
+class EvFilter(QtCore.QObject):
+    def eventFilter(self, _, event):
+        if event.type() != QtCore.QEvent.KeyPress:
+            return False
+        if aqt.mw.state != 'review':
+            return False
+        if aqt.mw.reviewer.state != 'answer':
+            return False
+        if event.isAutoRepeat():
+            return False
+        if event.spontaneous():
+            return False
+        if event.key() != QtCore.Qt.Key_K:
+            return False
+        try:
+            card = aqt.mw.reviewer.card
+            note = card.note()
+            expression = note['Expression']
+            play_random_sentence(expression)
+        except Exception:
+            pass
+        return True
+
+
+JPN_VOICES = get_voices('ja_')
+ENG_VOICES = get_voices('my name is')
+
+aqt.mw.installEventFilter(EvFilter(parent=aqt.mw))
