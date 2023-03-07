1dbe2ac635922fc41c79eb679149733ec1762c3b
Erez Volk <erez.volk@gmail.com>
2023-03-07 17:43:46 +0200

    pdf2utf: sections

--- attic/pdf2utf.py
+++ attic/pdf2utf.py
@@ -20,7 +20,8 @@ except ImportError:
     HAVE_GOOGLE = False
 
 
-PAUSE_TO_GAP_MS = {2: 3000, 1: 1000, 0: 0}
+PAUSE_TO_GAP_MS = {2: 3000, 1: 1000}
+PAUSE_TO_MIN_WORDS = {2: 5, 1: 10}
 THIS = Path(__file__).stem
 NON_LOWER_RE = r"[^a-z\xDF-\xF6\xF8-\xFF]"
 HEADER_RE = f"{NON_LOWER_RE}*[A-Z]{NON_LOWER_RE}*"
@@ -31,7 +32,7 @@ class Pdf2Utf:
     """PDF to UTF-8 and audiobook (WIP)"""
     parser: argparse.ArgumentParser
     args: argparse.Namespace
-    gg_client: "gg_tts.TextToSpeechClient" | None = None  # TextToSpeechClient
+    gg_client: object | None = None  # TextToSpeechClient
     gg_voice: object  # VoiceSelectionParams
     gg_audio: object  # AudioConfig
 
@@ -75,7 +76,7 @@ class Pdf2Utf:
         parser.add_argument(
             "--force", action="store_true", help="Generate audio even if file exists"
         )
-        parser.add_argument("-S", "--suffix", default="flac")
+        parser.add_argument("-S", "--suffix", default="mp3")
         self.parser = parser
         self.args = parser.parse_args()
 
@@ -217,8 +218,22 @@ class Pdf2Utf:
 
         paras = book.groupby("para").text.apply(" ".join).to_frame("text")
         paras.set_index(np.arange(len(paras)) + 1, inplace=True)
-        paras["pause"] = book[para_tmap].pause.shift(-1, fill_value=0).to_numpy()
-        paras["n_words"] = book.text.str.split(r"\s", regex=True).str.len()
+        paras["pause"] = book[para_tmap].pause.shift(-1, fill_value=2).to_numpy()
+        paras["n_words"] = paras.text.str.split(r"\s", regex=True).str.len()
+
+        # Trivial section splitting
+        curr_sect = 1
+        curr_n_words = 0
+        sects = []
+        for para, row in paras.iterrows():
+            curr_n_words += row.n_words
+            if curr_n_words >= PAUSE_TO_MIN_WORDS[row.pause]:
+                curr_sect += 1
+                curr_n_words = 0
+            sects.append(curr_sect)
+        paras["sect"] = sects
+        paras["sect_words"] = paras.groupby("sect").n_words.transform("sum")
+
         self.ddump(paras, "paras")
 
         print(f"Writing {self.args.output}")
@@ -284,19 +299,24 @@ class Pdf2Utf:
         if self.args.max_paragraphs:
             tosay = tosay.iloc[: self.args.max_paragraphs]
 
+        desc = [f"paragraphs: {len(tosay)}"]
         if len(tosay) < len(paras):
-            print(f"TTS (paragraphs: {len(tosay)} of {len(paras)})")
-        else:
-            print(f"TTS (paragraphs: {len(tosay)})")
+            desc.append(f" of {len(paras)}")
+        desc.append(f"; sections: {tosay.sect.nunique()}")
+        print(f"TTS ({''.join(desc)})")
         stem = self.args.stem or self.args.output.stem
-        for para, row in tosay.iterrows():
-            path = self.args.output.with_name(f"{stem}-{para:04d}.{self.args.suffix}")
+        for sect, group in tosay.groupby("sect"):
+            path = self.args.output.with_name(f"{stem}-{sect:04d}.{self.args.suffix}")
             if self.args.force or not path.is_file():
-                tts(text=row.text, pause=row.pause, path=path)
+                tts(rows=group, path=path)
 
-    def macos_tts(self, text: str, pause: int, path: Path):
+    def macos_tts(self, rows: pd.DataFrame, path: Path):
         """TTS using MacOS's `say (1)` and `sox`."""
         aiff = path.with_suffix(".aiff")
+        text = " ".join(
+            f"[[slnc 600]] {row.text} [[slnc {PAUSE_TO_GAP_MS[row.pause] - 600}]]"
+            for _, row in rows.iterrows()
+        )
         subprocess.run(
             [
                 "say",
@@ -304,7 +324,7 @@ class Pdf2Utf:
                 self.args.macos_voice,
                 "-o",
                 str(aiff),
-                f"[[slnc 600]] " f"{text} " f"[[slnc {PAUSE_TO_GAP_MS[pause] - 600}]]",
+                text,
             ],
             check=True,
         )
@@ -330,7 +350,7 @@ class Pdf2Utf:
                     "".join(voice.language_codes),
                 )
 
-    def google_tts(self, text: str, pause: int, path: Path):
+    def google_tts(self, rows: pd.DataFrame, path: Path):
         """TTS using Google cloud."""
         if self.gg_client is None:
             name = self.args.google_voice
@@ -339,23 +359,24 @@ class Pdf2Utf:
                 name=name,
                 language_code=re.sub(r"^([a-z][a-z]-[A-Z][A-Z])", r"\1", name),
             )
-            self.gg_audio = (
-                gg_tts.AudioConfig(audio_encoding=gg_tts.AudioEncoding.OGG_OPUS),
-            )
+            if self.args.suffix == ".mp3":
+                audio_encoding = gg_tts.AudioEncoding.MP3
+            else:
+                audio_encoding = gg_tts.AudioEncoding.OGG_OPUS
+            self.gg_audio = gg_tts.AudioConfig(audio_encoding=audio_encoding)
 
-        parts = [
-            "<speak>",
-            html.escape(text),
-            "</speak>",
-            f"""<break time="{PAUSE_TO_GAP_MS[pause]}ms"/>""",
-        ]
+        ssml = "".join(
+            f"""<speak>{html.escape(row.text)}</speak>"""
+            f"""<break time="{PAUSE_TO_GAP_MS[row.pause]}ms"/>"""
+            for _, row in rows.iterrows()
+        )
 
         ogg = path.with_suffix(".ogg")
         response = self.gg_client.synthesize_speech(
             request={
                 "voice": self.gg_voice,
                 "audio_config": self.gg_audio,
-                "input": gg_tts.SynthesisInput(ssml="".join(parts)),
+                "input": gg_tts.SynthesisInput(ssml=ssml),
             }
         )
 
