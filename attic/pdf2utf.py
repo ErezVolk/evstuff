ecc5e6de6975576859c00c61a1f05302ec80cc21
Erez Volk <erez.volk@gmail.com>
2023-03-08 18:47:39 +0200

    Better Google TTS

--- attic/pdf2utf.py
+++ attic/pdf2utf.py
@@ -20,8 +20,8 @@ except ImportError:
     HAVE_GOOGLE = False
 
 
-PAUSE_TO_GAP_MS = {2: 3000, 1: 1000}
-PAUSE_TO_MIN_WORDS = {2: 5, 1: 20}
+PAUSE_TO_GAP_MS = {3: 4000, 2: 3000, 1: 1000}
+PAUSE_TO_MIN_WORDS = {3: 1, 2: 5, 1: 20}
 THIS = Path(__file__).stem
 NON_LOWER_RE = r"[^a-z\xDF-\xF6\xF8-\xFF]"
 HEADER_RE = f"{NON_LOWER_RE}*[A-Z]{NON_LOWER_RE}*"
@@ -32,9 +32,10 @@ class Pdf2Utf:
     """PDF to UTF-8 and audiobook (WIP)"""
     parser: argparse.ArgumentParser
     args: argparse.Namespace
-    gg_client: object | None = None  # TextToSpeechClient
-    gg_voice: object  # VoiceSelectionParams
-    gg_audio: object  # AudioConfig
+    gg_client: object | None = None
+    gg_voice: object
+    gg_audio: object
+    ddump_no: int = 0
 
     def parse_args(self):
         """Command-line arguments"""
@@ -158,12 +159,8 @@ class Pdf2Utf:
         # Remove empty lines
         book.drop(book.index[book.text == ""], inplace=True)
 
-        # Remove line numbers (TODO: risky, insufficient)
+        # Remove line numbers (TODO: Allende, risky, insufficient)
         book.drop(book.index[book.text == book.page_label], inplace=True)
-
-        # Allende-specific
-        book.drop(book.index[book.text.str.contains("\n")], inplace=True)
-        book.drop(book.index[book.bigness == "l"], inplace=True)
 
         book["from_prev"] = book.groupby("page_no").bottom.diff()
 
@@ -198,17 +195,17 @@ class Pdf2Utf:
 
         self.ddump(book, "dehyphenated")
 
-        book.drop(book.index[book.bigness == "l"], inplace=True)  # Allende
-        book.drop(book.index[book.left_margin > 190], inplace=True)  # Allende
         book["from_left"] = book.left - book.left_margin
+
+        header_tmap = book.text.str.fullmatch(HEADER_RE)
+        header_tmap |= (book.bigness == "l") & (book.bigness.shift(1) != "l")
+        header_tmap &= book.first_in_page
+        book["header"] = header_tmap
 
         book["pause"] = 0
         book.loc[book.from_left > 15, "pause"] = 1  # Allende
         book.loc[book.from_prev > 30, "pause"] = 2  # Allende
-        book.loc[
-            book.first_in_page & book.text.str.fullmatch(HEADER_RE), "pause"
-        ] = 2  # Allende(-ish)
-        book.at[book.index[0], "pause"] = 2  # Always start with a bang
+        book.loc[header_tmap, "pause"] = 3  # Heuristic
         para_tmap = book.pause > 0
 
         book.loc[para_tmap, "para"] = np.arange(1, para_tmap.sum() + 1)
@@ -221,29 +218,18 @@ class Pdf2Utf:
         paras["pause"] = book[para_tmap].pause.shift(-1, fill_value=2).to_numpy()
         paras["n_words"] = paras.text.str.split(r"\s", regex=True).str.len()
 
-        # Trivial section splitting
-        curr_sect = 1  # TODO: para
-        curr_n_words = 0
-        sects = []
-        for para, row in paras.iterrows():
-            curr_n_words += row.n_words
-            if curr_n_words >= PAUSE_TO_MIN_WORDS[row.pause]:
-                curr_sect += 1  # TODO: PARA!!!
-                curr_n_words = 0
-            sects.append(curr_sect)
-        paras["sect"] = sects
-        paras["sect_words"] = paras.groupby("sect").n_words.transform("sum")
-
+        # Chapter numbers (of a sort)
+        paras["header"] = book[para_tmap].header.to_numpy()
+        paras.at[paras.index[0], "header"] = True
+        paras.loc[paras.header, "chap"] = np.arange(paras.header.sum()) + 1
+        paras.chap = paras.chap.fillna(method="pad").astype(int)
         self.ddump(paras, "paras")
 
         print(f"Writing {self.args.output}")
         with open(self.args.output, "w", encoding="UTF-8") as fobj:
             for _, row in paras.iterrows():
                 fobj.write(row.text)
-                if row.pause == 2:
-                    fobj.write("\n\n\n\n")
-                elif row.pause == 1:
-                    fobj.write("\n\n")
+                fobj.write("\n\n" * row.pause)
 
         if self.args.macos_voice:
             self.convert(paras, self.macos_tts)
@@ -283,9 +269,7 @@ class Pdf2Utf:
         """Convert text to audio"""
         tosay = paras
         if self.args.first_prefix:
-            self.args.first_paragraph = paras.index[
-                paras.text.str.startswith(self.args.first_prefix)
-            ][0]
+            self.args.first_paragraph = paras.index[paras.text.str.startswith(self.args.first_prefix)][0]
         if self.args.first_paragraph:
             tosay = tosay.loc[tosay.index >= self.args.first_paragraph]
 
@@ -302,11 +286,11 @@ class Pdf2Utf:
         desc = [f"paragraphs: {len(tosay)}"]
         if len(tosay) < len(paras):
             desc.append(f" of {len(paras)}")
-        desc.append(f"; sections: {tosay.sect.nunique()}")
+        desc.append(f"; chapters: {tosay.chap.nunique()}")
         print(f"TTS ({''.join(desc)})")
         stem = self.args.stem or self.args.output.stem
-        for sect, group in tosay.groupby("sect"):
-            path = self.args.output.with_name(f"{stem}-{sect:04d}.{self.args.suffix}")
+        for chap, group in tosay.groupby("chap"):
+            path = self.args.output.with_name(f"{stem}-{chap:04d}.{self.args.suffix}")
             if self.args.force or not path.is_file():
                 tts(rows=group, path=path)
 
@@ -351,7 +335,11 @@ class Pdf2Utf:
                 )
 
     def google_tts(self, rows: pd.DataFrame, path: Path):
-        """TTS using Google cloud."""
+        """TTS using Google cloud.
+
+        There's a "synthesize long audio" API, but that requires creating
+        a bucket and all that.
+        """
         if self.gg_client is None:
             name = self.args.google_voice
             self.gg_client = gg_tts.TextToSpeechClient()
@@ -359,38 +347,33 @@ class Pdf2Utf:
                 name=name,
                 language_code=re.sub(r"^([a-z][a-z]-[A-Z][A-Z])", r"\1", name),
             )
-            if self.args.suffix == ".mp3":
-                audio_encoding = gg_tts.AudioEncoding.MP3
-            else:
-                audio_encoding = gg_tts.AudioEncoding.OGG_OPUS
-            self.gg_audio = gg_tts.AudioConfig(audio_encoding=audio_encoding)
+            self.gg_audio = gg_tts.AudioConfig(audio_encoding=gg_tts.AudioEncoding.MP3)
 
-        ssml = "".join(
-            f"""<speak>{html.escape(row.text)}</speak>"""
-            f"""<break time="{PAUSE_TO_GAP_MS[row.pause]}ms"/>"""
-            for _, row in rows.iterrows()
-        )
+        mp3 = path.with_suffix(".mp3")
+        print(f"{path.name} (words={rows.n_words.sum()}) ...")
+        with open(mp3, "wb") as ofo:
+            for _, row in rows.iterrows():
+                ssml = (
+                    f'<speak>{html.escape(row.text)}</speak>'
+                    f'<break time="{PAUSE_TO_GAP_MS[row.pause]}ms"/>'
+                )
+                response = self.gg_client.synthesize_speech(request={
+                    "voice": self.gg_voice,
+                    "audio_config": self.gg_audio,
+                    "input": gg_tts.SynthesisInput(ssml=ssml),
+                })
+                ofo.write(response.audio_content)
 
-        ogg = path.with_suffix(".ogg")
-        response = self.gg_client.synthesize_speech(
-            request={
-                "voice": self.gg_voice,
-                "audio_config": self.gg_audio,
-                "input": gg_tts.SynthesisInput(ssml=ssml),
-            }
-        )
-
-        with open(ogg, "wb") as ofo:
-            ofo.write(response.audio_content)
-
-        self.sox_and_rm(ogg, path)
+        self.sox_and_rm(mp3, path)
+        print(f"... {path.stat().st_size:,} Bytes")
 
     def ddump(self, obj, name: str):
         """Debug dump of an object"""
         if not self.args.debug:
             return
 
-        path = self.args.output.parent / f"{THIS}-{name}"
+        self.ddump_no += 1
+        path = self.args.output.parent / f"{THIS}-{self.ddump_no:02d}-{name}"
         if isinstance(obj, pd.DataFrame):
             path = path.with_suffix(".parquet")
             print(f"(debug) Writing {path}")
