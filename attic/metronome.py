7705ef11e37e8e67def98ee1336076376c8f05a6
Erez Volk <erez.volk@gmail.com>
2023-10-07 11:52:14 +0300

    Working on pause/play

--- attic/metronome.py
+++ attic/metronome.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import argparse
-import logging
 from pathlib import Path
 import re
 import signal
@@ -8,33 +7,38 @@ import time
 import wave
 
 import pyaudio
-import rich.logging
+import pynput.keyboard
+import Quartz
 
 # TODO: Better than assert
 # TODO: Support non-wav clicks
 # TODO: Handle incompatible hi/lo
 # TODO: memory
 # TODO: interactive (tempo up/down...), maybe with Textual
-# TODO: tempo=500
+# TODO: support tempo=500
 
 
 # Metronome sounds recorded by Ludwig Peter Müller (muellerwig@gmail.com)
 # and distributed under the Creative Commons CC0 1.0 Universal license.
-# Original post: https://www.reddit.com/r/audioengineering/comments/kg8gth/free_click_track_sound_archive/?rdt=48837
+# https://www.reddit.com/r/audioengineering/comments/kg8gth/free_click_track_sound_archive/?rdt=48837
 # Zip file: https://stash.reaper.fm/40824/Metronomes.zip
 
 THIS = Path(__file__).resolve()
 HERE = THIS.parent
 
+# Media keys from <hidsystem/ev_keymap.h>
+NX_KEYTYPE_EJECT = 14
+NX_KEYTYPE_PLAY = 16
+
 
 class Metronome:
     args: argparse.Namespace
-    log: logging.Logger
     bytes_per_frame: int
     loop_size: int
     loop: bytearray
     offset: int
-    stopping: bool = False
+    aborting: bool = False
+    paused: bool = False
 
     def parse_args(self):
         parser = argparse.ArgumentParser()
@@ -83,39 +87,14 @@ class Metronome:
 
         assert self.args.tempo > 0
 
-    def configure_logging(self):
-        """Set logging format and level"""
-        if self.args.debug:
-            level = logging.DEBUG
-        else:
-            level = logging.INFO
-        logging.basicConfig(
-            format="%(message)s",
-            datefmt="[%X]",
-            level=level,
-            handlers=[rich.logging.RichHandler()],
-        )
-        self.log = logging.getLogger(THIS.stem)
-
     def run(self):
         self.parse_args()
-        self.configure_logging()
 
-        with wave.open(str(self.args.click_hi), "rb") as wfo:
-            channels = wfo.getnchannels()
-            rate = wfo.getframerate()
-            width = wfo.getsampwidth()
-            hi_data = wfo.readframes(wfo.getnframes())
-        with wave.open(str(self.args.click_lo), "rb") as wfo:
-            assert channels == wfo.getnchannels()
-            assert rate == wfo.getframerate()
-            assert width == wfo.getsampwidth()
-            lo_data = wfo.readframes(wfo.getnframes())
+        self.read_clicks()
 
         beat_sec = 60 / self.args.tempo
-        self.bytes_per_frame = channels * width
 
-        self.log.info(f"Tempo: ♩ = {self.args.tempo}")
+        print(f"Tempo: ♩ = {self.args.tempo}")
         los = []
         if self.args.subdivision:
             assert re.fullmatch(r"[-tT]+", self.args.subdivision)
@@ -149,7 +128,7 @@ class Metronome:
             his = [.5, 1, 2, 2.75, 3.5]
             los = []
         else:
-            self.log.info(f"Beat: {self.args.beat}")
+            print(f"Beat: {self.args.beat}")
             assert all(beat > 0 for beat in self.args.beat)
             his = []
             beats_per_bar = 0
@@ -160,11 +139,11 @@ class Metronome:
 
         events = {
             pos: data
-            for data, positions in ((lo_data, los), (hi_data, his))
+            for data, positions in ((self.lo_data, los), (self.hi_data, his))
             for pos in positions
         }
 
-        ideal_bar = beats_per_bar * beat_sec * rate * self.bytes_per_frame
+        ideal_bar = beats_per_bar * beat_sec * self.rate * self.bytes_per_frame
         bars_per_loop = 1
         rounded_frames = round(ideal_bar / self.bytes_per_frame)
         self.loop_size = rounded_frames * self.bytes_per_frame
@@ -173,36 +152,61 @@ class Metronome:
         for n_bar in range(bars_per_loop):
             for pos, data in events.items():
                 beat_n = (n_bar * beats_per_bar) + pos
-                offset = round(beat_n * beat_sec * rate) * self.bytes_per_frame
+                offset = round(beat_n * beat_sec * self.rate) * self.bytes_per_frame
                 self.loop[offset:offset + len(data)] = data
         self.offset = 0
 
         if self.args.debug:
             with wave.open("loop.wav", "wb") as wfo:
-                wfo.setnchannels(channels)
-                wfo.setframerate(rate)
-                wfo.setsampwidth(width)
+                wfo.setnchannels(self.channels)
+                wfo.setframerate(self.rate)
+                wfo.setsampwidth(self.width)
                 wfo.writeframes(self.loop)
 
-        signal.signal(signal.SIGINT, self.stop)
+        # Exit cleanly on Ctrl-C
+        signal.signal(signal.SIGINT, self.handle_ctrl_c)
+
+        # Listen to Media keys
+        listener = pynput.keyboard.Listener(darwin_intercept=self.intercept)
+        listener._EVENTS = Quartz.CGEventMaskBit(Quartz.NSSystemDefined)
+        listener.start()
 
         player = pyaudio.PyAudio()
-        stream = player.open(
-            format=player.get_format_from_width(width),
-            channels=channels,
-            rate=rate,
-            output=True,
-            stream_callback=self.read_more,
-        )
 
-        while stream.is_active():
-            time.sleep(.1)
+        while not self.aborting:
+            stream = player.open(
+                format=player.get_format_from_width(self.width),
+                channels=self.channels,
+                rate=self.rate,
+                output=True,
+                stream_callback=self.read_more,
+            )
 
-        stream.close()
+            while stream.is_active():
+                time.sleep(.1)
+
+            stream.close()
+
+            while self.paused and not self.aborting:
+                time.sleep(.1)
+
         player.terminate()
 
+    def read_clicks(self):
+        with wave.open(str(self.args.click_hi), "rb") as wfo:
+            self.channels = wfo.getnchannels()
+            self.rate = wfo.getframerate()
+            self.width = wfo.getsampwidth()
+            self.hi_data = wfo.readframes(wfo.getnframes())
+        with wave.open(str(self.args.click_lo), "rb") as wfo:
+            assert self.channels == wfo.getnchannels()
+            assert self.rate == wfo.getframerate()
+            assert self.width == wfo.getsampwidth()
+            self.lo_data = wfo.readframes(wfo.getnframes())
+        self.bytes_per_frame = self.channels * self.width
+
     def read_more(self, in_data, frame_count, time_info, status):
-        if self.stopping:
+        if self.aborting or self.paused:
             return (b'', pyaudio.paAbort)
 
         nbytes = frame_count * self.bytes_per_frame
@@ -217,9 +221,34 @@ class Metronome:
         data = b''.join(chunks)
         return (data, pyaudio.paContinue)
 
-    def stop(self, sig, frame):
-        self.log.info("Aborting...")
-        self.stopping = True
+    def handle_ctrl_c(self, sig, frame):
+        """Handle Ctrl-C"""
+        self.abort()
+
+    def abort(self):
+        print("Aborting...")
+        self.aborting = True
+
+    def intercept(self, event_type, event_ref):
+        """Handle media key event"""
+        event = Quartz.NSEvent.eventWithCGEvent_(event_ref)
+
+        bitmap = event.data1()
+        key = (bitmap & 0xffff0000) >> 16
+        is_press = ((bitmap & 0xff00) >> 8) == 0x0a
+
+        if key == NX_KEYTYPE_EJECT:
+            if not is_press:
+                self.abort()
+            return None
+
+        if key == NX_KEYTYPE_PLAY:
+            if not is_press:
+                self.paused = not self.paused
+                print(f"To {'restart' if self.paused else 'pause'}, press ⏯")
+            return None
+
+        return event_ref
 
 
 if __name__ == "__main__":
