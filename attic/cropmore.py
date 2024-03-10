63f43ad41e5a92f5a368a33a7313723baf8f0ced
Erez Volk <erez.volk@gmail.com>
2024-03-10 22:33:51 +0200

    lint (cropmore)

--- attic/cropmore.py
+++ attic/cropmore.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Crop PDF even more"""
+"""Crop PDF even more."""
 import argparse
 from pathlib import Path
 
@@ -7,26 +7,42 @@ import fitz
 
 
 class CropMore:
-    """Crop PDF even more"""
+    """Crop PDF even more."""
+
     parser: argparse.ArgumentParser
     args: argparse.Namespace
     cropping: bool
 
-    def parse_args(self):
-        """Command-line arguments"""
+    def parse_args(self) -> None:
+        """Parse command-line arguments."""
         parser = argparse.ArgumentParser()
         parser.add_argument("input", type=Path, nargs="?", help="Input PDF")
         parser.add_argument("output", type=Path, nargs="?", help="Output PDF")
         parser.add_argument("-f", "--first-page", type=int, help="First page")
         parser.add_argument("-l", "--last-page", type=int, help="Last page")
-        parser.add_argument("-M", "--media", action="store_true", help="Use mediabox as basis for cropping")
-        parser.add_argument("-A", "--all", type=int, default=0, help="Crop on all sides (--top etc. override this)")
+        parser.add_argument(
+            "-M",
+            "--media",
+            action="store_true",
+            help="Use mediabox as basis for cropping",
+        )
+        parser.add_argument(
+            "-A",
+            "--all",
+            type=int,
+            default=0,
+            help="Crop on all sides (--top etc. override this)",
+        )
         parser.add_argument("-T", "--top", type=int, help="Top crop")
         parser.add_argument("-B", "--bottom", type=int, help="Bottom crop")
         parser.add_argument("-L", "--left", type=int, help="Left crop")
         parser.add_argument("-R", "--right", type=int, help="Right crop")
-        parser.add_argument("-p", "--pixmap", action="store_true", help="Convert to graphics")
-        parser.add_argument("-D", "--dpi", type=int, default=600, help="Used when --pixmap")
+        parser.add_argument(
+            "-p", "--pixmap", action="store_true", help="Convert to graphics",
+        )
+        parser.add_argument(
+            "-D", "--dpi", type=int, default=600, help="Used when --pixmap",
+        )
 
         args = parser.parse_args()
 
@@ -55,8 +71,8 @@ class CropMore:
         self.parser = parser
         self.args = args
 
-    def main(self):
-        """Do it"""
+    def main(self) -> None:
+        """Do it."""
         self.parse_args()
 
         print(f"`{self.args.input}' -> `{self.args.output}'")
@@ -72,12 +88,13 @@ class CropMore:
         if self.cropping:
             for page in odoc:
                 box = page.mediabox if self.args.media else page.cropbox
-                page.set_cropbox(fitz.Rect(
+                crop = fitz.Rect(
                     box.x0 + self.args.left,
                     box.y0 + self.args.top,
                     box.x1 - self.args.right,
                     box.y1 - self.args.bottom,
-                ))
+                )
+                page.set_cropbox(crop)
 
         if self.args.pixmap:
             tdoc = odoc
