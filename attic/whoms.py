82a80f04fd9850db6916eeadb038d8a2f53f047b
Erez Volk <erez.volk@gmail.com>
2024-08-27 16:21:13 +0300

    whoms: correctly handle multi-whom albums

--- attic/whoms.py
+++ attic/whoms.py
@@ -19,7 +19,7 @@ def main() -> None:
         "-o",
         "--output",
         type=Path,
-        default=Path(__file__).with_suffix(".xlsx"),
+        default="whoms.xlsx",
     )
     parser.add_argument(
         "-N",
@@ -33,16 +33,28 @@ def main() -> None:
         type=int,
         default=4,
     )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+    )
     args = parser.parse_args()
 
     albums = pd.read_excel(args.input)
+    albums["heard"] = albums.When.notna()
+    albums["Whoms"] = albums.Whom.str.replace(
+        r"\s*[,&]\s*", "|", regex=True,
+    ).str.split("|")
 
-    whoms = albums.Whom.value_counts().to_frame("total")
-    whoms["heard"] = albums[albums.When.notna()].Whom.value_counts()
+    whalbums = albums.explode("Whoms")
+    whoms = whalbums.Whoms.value_counts().to_frame("total")
+    whoms["heard"] = whalbums[whalbums.heard].Whoms.value_counts()
     whoms["heard"] = whoms.heard.fillna(0).astype(int)
     whoms["cov"] = whoms.heard / whoms.total
     print(f"Writing {len(whoms)} whoms to {args.output}")
     whoms.to_excel(args.output)
+    if args.verbose:
+        print(whoms)
 
     unheard = albums[albums.When.isna()]
     shorts = unheard.sort_values("dt").iloc[:len(unheard) // args.shortest_nth]
@@ -50,16 +62,21 @@ def main() -> None:
     print(f"Some suggestions ({len(offer)} of {len(unheard)}):")
     for _, row in offer.iterrows():
         print(
-            f'- [{row.n}] "{row.What}" by {one_of(row.Who)} '
+            f'- [{row.n}] "{row.What}" ({row.t}) by {one_of(row.Who)} '
             f'(with {one_of(row.Whom)}, {row["dt"]})',
         )
+
+
+def normalize(series: pd.Series) -> pd.Series:
+    """Normalize all commas and ampersands to |s."""
+    return series.str.replace(r"\s*[,&]\s*", "|", regex=True)
 
 
 def one_of(names: str) -> str:
     """For a list of names, return the first one."""
     for sep in ",&":
         if (off := names.find(sep)) > 0:
-            return names[:off].strip() + " +"
+            return names[:off].strip() + " et al."
     return names
 
 
