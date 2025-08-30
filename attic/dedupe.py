3404ac50ca15a9211663695bab70ada81d733ff6
Erez Volk <erez.volk@gmail.com>
2025-08-30 10:08:31 +0300

    dedupe.py

--- attic/dedupe.py
+++ attic/dedupe.py
@@ -2,10 +2,14 @@
 """Make hard links where possible."""
 import argparse
 import collections
+import csv
 import hashlib
 import stat
+import subprocess
 import typing as t
 from pathlib import Path
+
+# TO DO: --exclude
 
 
 class File:
@@ -52,8 +56,11 @@ class Dedupe:
         """Dedupe."""
         parser = argparse.ArgumentParser()
         parser.add_argument("root", type=Path, default=Path.cwd())
+        parser.add_argument("-e", "--exclude", type=Path, nargs="+")
         parser.add_argument("-s", "--min-size", type=int, default=1024 * 1024)
         parser.add_argument("-y", "--yes", action="store_true")
+        parser.add_argument("-c", "--csv", type=Path, default="dedupe.csv")
+        parser.add_argument("-C", "--open-csv", action="store_true")
         args = parser.parse_args()
 
         inodes: dict[int, list[File]] = collections.defaultdict(list)
@@ -65,24 +72,36 @@ class Dedupe:
                 inodes[file.inode].append(file)
                 sizes[file.size].add(file.inode)
 
-        to_merge: list[list[File]] = []
-        for size, size_inodes in sizes.items():
-            if len(size_inodes) == 1:
-                continue  # Only one file of this size
-            hashes = {inodes[inode][0].hash() for inode in size_inodes}
-            if len(hashes) > 1:
-                continue  # Not all the same (TO DO: maybe there are subsets)
-            files = [
-                file
-                for inode in size_inodes
-                for file in inodes[inode]
-            ]
-            to_merge.append(files)
-            print(f"[{len(to_merge)}]", f"({size:,} B)", files)
+        total_save = 0
+        with Path(args.csv).open("w") as csvfo:
+            csvw = csv.writer(csvfo)
+            csvw.writerow(["count", "size", "save"])
+            to_merge: list[list[File]] = []
+            for size, size_inodes in sizes.items():
+                if len(size_inodes) == 1:
+                    continue  # Only one file of this size
+                hashes = {inodes[inode][0].hash() for inode in size_inodes}
+                if len(hashes) > 1:
+                    continue  # Not all the same (TO DO: maybe subsets)
+                files = [
+                    file
+                    for inode in size_inodes
+                    for file in inodes[inode]
+                ]
+                to_merge.append(files)
+                print(f"[{len(to_merge)}]", f"({size:,} B)", files)
+                count = len(files)
+                save = size * (count - 1)
+                total_save += save
+                csvw.writerow(map(str, [count, size, save, *files]))
 
         if not to_merge:
             return
 
+        print(f"Hard links would save {total_save:,} bytes")
+        print("See also", args.csv)
+        if args.open_csv:
+            subprocess.run(["/usr/bin/open", str(args.csv)], check=False)
         if not args.yes:
             input("Press Return to hard link...")
 
