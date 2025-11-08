6ecde13517c2066712024df56a13f72dc2364319
Erez Volk <erez.volk@gmail.com>
2025-11-08 17:44:00 +0100

    docxshuffle --min-length --max-length --keep-comments

--- attic/docxshuffle.py
+++ attic/docxshuffle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Shuffle paragraphs in a Word document"""
+"""Shuffle paragraphs in a Word document."""
 import argparse
 from collections import defaultdict
 from pathlib import Path
@@ -14,13 +14,13 @@ from docx_worker import DocxWorker
 
 
 class DocxShuffle(DocxWorker):
-    """Shuffle paragraphs in a Word document"""
+    """Shuffle paragraphs in a Word document."""
     args: argparse.Namespace
     body: etree._Entity
     rng: np.random.Generator
 
     def parse_args(self):
-        """Command-line options"""
+        """Parse command-line options."""
         parser = argparse.ArgumentParser()
         parser.add_argument(
             "input",
@@ -47,13 +47,29 @@ class DocxShuffle(DocxWorker):
             "--contains",
             metavar="REGEX",
         )
+        parser.add_argument(
+            "-l",
+            "--min-length",
+            type=int,
+            default=1,
+        )
+        parser.add_argument(
+            "-L",
+            "--max-length",
+            type=int,
+        )
+        parser.add_argument(
+            "-C",
+            "--keep-comments",
+            action="store_true",
+        )
         self.args = parser.parse_args()
         if self.args.input is None:
             docs = self.glob_docs()
             if len(docs) != 1:
                 parser.error(
                     f"{len(docs)} .docx files found, "
-                    f"please specify input"
+                    f"please specify input",
                 )
             self.args.input, = docs
             print(f"Working on the only .docx here, {self.args.input}")
@@ -69,6 +85,14 @@ class DocxShuffle(DocxWorker):
     def work(self):
         self.randomize()
 
+        if not self.args.keep_comments:
+            etree.strip_elements(
+                self.doc,
+                self.wtag("commentReference"),
+                self.wtag("commentRangeStart"),
+                self.wtag("commentRangeEnd"),
+            )
+
         num_pnodes = 0
         self.body = self.find(self.doc, "./w:body")
         pnodes = list(self.xpath(self.body, "./w:p"))
@@ -79,14 +103,20 @@ class DocxShuffle(DocxWorker):
         l2ps = defaultdict(list)
         for pnode in pnodes:
             text = "".join(tnode.text for tnode in self.pnode_tnodes(pnode))
+            if len(text) < self.args.min_length:
+                continue
+            if self.args.max_length:
+                if len(text) > self.args.max_length:
+                    continue
             if self.args.contains:
                 if not re.search(self.args.contains, text):
                     continue
             meat = re.sub(r"[^א-תa-zA-Z]", "", text)
-            if meat:
-                l2ps[meat].append(pnode)
-                meat_pnodes.append(pnode)
-                num_pnodes += 1
+            if not meat:
+                continue
+            l2ps[meat].append(pnode)
+            meat_pnodes.append(pnode)
+            num_pnodes += 1
 
         print(f"Number of unique lines: {len(l2ps)} of {num_pnodes}")
         new_pnodes = [
@@ -108,19 +138,25 @@ class DocxShuffle(DocxWorker):
             pnode
             for line, pnodes in sorted(
                 l2ps.items(),
-                key=lambda item: "".join(reversed(item[0]))
+                key=lambda item: "".join(reversed(item[0])),
             )
             for pnode in pnodes
         ])
         self.write_as("detros")
 
+        self.reorder([
+            pnodes[0]
+            for line, pnodes in sorted(l2ps.items(), key=lambda i: len(i[0]))
+        ])
+        self.write_as("growing")
+
         if not self.args.model:
             return
 
-        with open(self.args.model, "r", encoding="utf-8") as mfo:
+        with self.args.model.open("r", encoding="utf-8") as mfo:
             model_lines = []
-            for line in mfo:
-                line = line.strip()
+            for unstripped in mfo:
+                line = unstripped .strip()
                 if line and not line.startswith("#"):
                     line = re.sub(r"[^a-z ]", "", line.lower())
                     model_lines.append(line)
@@ -134,8 +170,8 @@ class DocxShuffle(DocxWorker):
         ])
         self.write_as("msort")
 
-    def color_background(self):
-        """Change paper color in the shuffled files"""
+    def color_background(self) -> None:
+        """Change paper color in the shuffled files."""
         if self.find(self.body, "./w:background") is None:
             self.body.append(self.make_w(
                 "background",
@@ -146,35 +182,35 @@ class DocxShuffle(DocxWorker):
                 },
             ))
 
-    def randomize(self):
-        """Read/Write random seed file"""
+    def randomize(self) -> None:
+        """Read/Write random seed file."""
         self.rng = np.random.default_rng()
         if self.args.one_time_seed:
             return
 
         seed_path = self.args.input.with_suffix(".seed")
         try:
-            with open(seed_path, "rb") as fobj:
+            with seed_path.open("rb") as fobj:
                 loaded = pickle.load(fobj)
                 if not isinstance(loaded, np.random.Generator):
                     print(f"Incompatible {seed_path}, regenerating")
-                    raise TypeError()
+                    raise TypeError
             print(f"Random state loaded from {seed_path}")
 
             self.rng = loaded
         except (FileNotFoundError, TypeError, pickle.UnpicklingError):
             print(f"Saving random state to {seed_path}")
-            with open(seed_path, "wb") as fobj:
+            with seed_path.open("wb") as fobj:
                 pickle.dump(self.rng, fobj)
 
-    def reorder(self, pnodes):
-        """Replace paragraphs with new order"""
+    def reorder(self, pnodes: list[etree._Entity]) -> None:
+        """Replace paragraphs with new order."""
         for pnode in list(self.xpath(self.body, "./w:p")):
             self.body.remove(pnode)
         self.body.extend(pnodes)
 
-    def write_as(self, suffix: str):
-        """Write a version of the file"""
+    def write_as(self, suffix: str) -> None:
+        """Write a version of the file."""
         path = self.args.input.with_stem(f"{self.args.stem}-{suffix}")
         print(f"Writing {path}")
         self.write(path)
