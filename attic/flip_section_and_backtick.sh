198fde1c4a8686f98779038fc24d8f39ed57b5de
Erez Volk <erez.volk@hexight.io>
2026-06-04 11:45:34 +0200

    Flip keys script

---
+++ attic/flip_section_and_backtick.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "This lasts only until restart. Follow the instructions in com.erezvolk.flipkeys.plist to run on login."
+
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
