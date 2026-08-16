96002f0a38d8c3c170afc7a986d57b00ae2d1faa
Erez Volk <erez.volk+ext@gmail.com>
2026-08-16 11:10:54 +0300

    Flip but not too much

--- attic/flip_section_and_backtick.sh
+++ attic/flip_section_and_backtick.sh
@@ -2,4 +2,6 @@
 
 echo "This lasts only until restart. Follow the instructions in com.erezvolk.flipkeys.plist to run on login."
 
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
+/usr/bin/hidutil property \
+    --matching '[{"Product":"Apple Internal Keyboard / Trackpad"},{"Product":"Magic Keyboard"},{"Product":"Magic Keyboard with Numeric Keypad"},{"Product":"Magic Keyboard with Touch ID"},{"Product":"Magic Keyboard with Touch ID and Numeric Keypad"}]' \
+    --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
